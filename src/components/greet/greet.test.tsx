99af8d1d7533ebd1347bbee9ec783e98d68a0376
Neha Goel <neha.goel@mindfiresolutions.com>
2023-05-16 15:50:17 +0530

    tests written according to TDD approach

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,8 +1,16 @@
+// Greet should render the text hello and if a name is passed into the Component, it should render hello followed by the name
+
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
 test("Greet renders correctly", () => {
 	render(<Greet/>);
-	const textElement = screen.getByText(/hello/i); // i denotes to ignore the case
+	const textElement = screen.getByText('Hello'); // i denotes to ignore the case
+	expect(textElement).toBeInTheDocument();
+})
+
+test("Greet renders with a name", () => {
+	render(<Greet name='Vishwas'/>);
+	const textElement = screen.getByText('Hello Vishwas');
 	expect(textElement).toBeInTheDocument();
 })
