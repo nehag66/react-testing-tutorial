f381bca4201d7036d0e85a8a209c68b62ca948f8
Neha Goel <neha.goel@mindfiresolutions.com>
2023-05-16 16:59:04 +0530

    code coverage parameters added for tests

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -3,14 +3,19 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
-test("Greet renders correctly", () => {
-	render(<Greet/>);
-	const textElement = screen.getByText('Hello'); // i denotes to ignore the case
-	expect(textElement).toBeInTheDocument();
+describe('Greet', () => {
+	test("renders correctly", () => {
+		render(<Greet/>);
+		const textElement = screen.getByText(/Hello/i); // i denotes to ignore the case
+		expect(textElement).toBeInTheDocument();
+	})
 })
 
-test("Greet renders with a name", () => {
-	render(<Greet name='Vishwas'/>);
-	const textElement = screen.getByText('Hello Vishwas');
-	expect(textElement).toBeInTheDocument();
+describe('Another describe method', () => {
+	test("renders with a name", () => {
+		render(<Greet name='Vishwas'/>);
+		const textElement = screen.getByText('Hello Vishwas');
+		expect(textElement).toBeInTheDocument();
+	})
 })
+
