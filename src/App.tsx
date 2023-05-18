2c8119a863913c6970deb861e0a6842caa843b11
Neha Goel <neha.goel@mindfiresolutions.com>
2023-05-18 21:12:04 +0530

    mouse and keyboard events

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Application } from './components/application/application';
+import { Skills } from './components/skills/skills';
+import { Counter } from './components/counter/counter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		  <div className="App">
+			{/* <Application />
+			<Skills skills={['HTML', 'CSS']} /> */}
+			<Counter />
+			{/* <CounterTwo count={1} />
+			<Users />
+			<MuiMode /> */}
+		  </div>
+		// </AppProviders>
+	  )
 }
 
 export default App;
