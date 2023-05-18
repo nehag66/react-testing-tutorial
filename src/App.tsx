e001983ee7cca6ed00a3f75db0e82353a68ce3a9
Neha Goel <neha.goel@mindfiresolutions.com>
2023-05-18 21:39:46 +0530

    mui

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import './App.css';
 import { Application } from './components/application/application';
 import { Skills } from './components/skills/skills';
 import { Counter } from './components/counter/counter';
+import { AppProviders } from './providers/providers';
+import { MuiMode } from './components/mui/muiMode';
 
 function App() {
 	return (
-		  <div className="App">
-			{/* <Application />
-			<Skills skills={['HTML', 'CSS']} /> */}
-			<Counter />
-			{/* <CounterTwo count={1} />
-			<Users />
-			<MuiMode /> */}
-		  </div>
-		// </AppProviders>
-	  )
+		<AppProviders>
+			<div className="App">
+				{/* <Application />
+				<Skills skills={['HTML', 'CSS']} /> */}
+				{/* <Counter /> */}
+				{/* <CounterTwo count={1} />
+				<Users />
+				 */}
+				<MuiMode />
+			</div>
+		</AppProviders>
+	)
 }
 
 export default App;
