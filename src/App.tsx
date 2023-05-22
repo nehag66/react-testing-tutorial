0211603de6054c1997d8bdab1d2c34fe6ec99e91
Neha Goel <neha.goel@mindfiresolutions.com>
2023-05-22 08:44:12 +0530

    mocking

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,19 @@ import { Skills } from './components/skills/skills';
 import { Counter } from './components/counter/counter';
 import { AppProviders } from './providers/providers';
 import { MuiMode } from './components/mui/muiMode';
+import { CounterTwo } from './components/counter-two/counter-two';
+import { Users } from './components/users/users';
 
 function App() {
 	return (
 		<AppProviders>
 			<div className="App">
-				{/* <Application />
-				<Skills skills={['HTML', 'CSS']} /> */}
+				{/* <Application /> */}
+				{/* <Skills skills={['HTML', 'CSS']} /> */}
 				{/* <Counter /> */}
-				{/* <CounterTwo count={1} />
+				<CounterTwo count={1} />
 				<Users />
-				 */}
-				<MuiMode />
+				{/* <MuiMode /> */}
 			</div>
 		</AppProviders>
 	)
